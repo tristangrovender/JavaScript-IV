67d7a3cb00b7e774a801d05edf3a0fde0d2ba758
Tristan Grovender <therealtristangrovender@gmail.com>
2019-10-28 13:27:11 -0500

    person base class added.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+class Person {
+	constructor(personAttr) {
+		this.name = personAttr.name;
+		this.age = personAttr.age;
+		this.location = personAttr.location;
+	}
+	speak() {
+		return console.log(
+			`Hello my name is ${this.name}, I am from ${this.location}`
+		);
+	}
+}
